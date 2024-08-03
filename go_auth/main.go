32a44682aa5a2b183a80dc836b526380d25b8b6d
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-08-03 10:58:27 +0300

    finished registeration with jwt locally

--- go_auth/main.go
+++ go_auth/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+type User struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
 
 func main() {
+
+	var userStore = make(map[string]*User)
+
 	router := gin.Default()
+	router.POST("/register", func(ctx *gin.Context) {
+		var user User
+		if err := ctx.ShouldBindJSON(&user); err!=nil{
+			ctx.JSON(400, gin.H{"error":"incorrect input"})
+			return
+		}
+
+		bytePassword := []byte(user.Password)
+
+		hashedUser, err := bcrypt.GenerateFromPassword(bytePassword,bcrypt.DefaultCost)
+		if err!=nil{
+			ctx.JSON(500, gin.H{"message":"internal error"})
+			return
+
+		}
+
+		user.Password = string(hashedUser)
+		userStore[user.Email] = &user
+		ctx.JSON(200, gin.H{"message":"user successfully registered!", "user":user})
+
+	})
+	router.Run("localhost:8080")
 }
