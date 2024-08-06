98df669e49b2fff959d474e843fadeca27b6e1a2
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-08-06 17:00:26 +0300

    finish task usecase

---
+++ task_management_with_clean_architecture/usecases/task_usecase.go
@@ -0,0 +1,42 @@
+package usecases
+
+import (
+	"task_with_clean_arc/domain"
+	"task_with_clean_arc/interfaces/repository"
+)
+
+type TaskUsecase interface {
+	GetTasks() ([]domain.Task, error)
+	GetTaskByID(id string) (domain.Task, error)
+	AddTask(task domain.Task) error
+	DeleteTask(id string) error
+	UpdateTask(id string, task domain.Task) error
+}
+
+type taskUsecase struct{
+	repo repository.TaskRepository
+}
+
+func NewTaskUsecase(repo repository.TaskRepository) TaskUsecase{
+	return &taskUsecase{repo: repo}
+}
+
+func (u *taskUsecase) GetTasks() ([]domain.Task, error){
+	return u.repo.GetAll()
+}
+
+func (u *taskUsecase) GetTaskByID(id string) (domain.Task, error){
+	return u.repo.GetOne(id)
+}
+
+func (u *taskUsecase) AddTask(task domain.Task) error{
+	return u.repo.Add(task)
+}
+
+func (u *taskUsecase) DeleteTask(id string) error{
+	return u.repo.Delete(id)
+}
+
+func (u *taskUsecase) UpdateTask(id string, task domain.Task) error{
+	return u.repo.Update(id,task)
+}
