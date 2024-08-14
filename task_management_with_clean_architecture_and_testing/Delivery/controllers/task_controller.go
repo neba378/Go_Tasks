fff04d96b9e06d2ff7552b137dedcae565575202
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-08-14 11:43:46 +0300

    modify the task controller to make it easy for testing

--- task_management_with_clean_architecture_and_testing/Delivery/controllers/task_controller.go
+++ task_management_with_clean_architecture_and_testing/Delivery/controllers/task_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"task_with_clean_arc_and_test/domain"
 	"task_with_clean_arc_and_test/usecases"
@@ -19,9 +20,10 @@ func NewTaskHandler(usecase usecases.TaskUsecase) *TaskHandler {
 func (h *TaskHandler) GetTasks(c *gin.Context) {
 	tasks, err := h.usecase.GetTasks()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to retrieve tasks"})
+		return
 	}
-	c.JSON(http.StatusOK, tasks)
+	c.JSON(http.StatusOK, tasks) // Ensure that only the tasks slice is returned
 }
 
 func (h *TaskHandler) GetTaskByID(c *gin.Context) {
@@ -29,6 +31,7 @@ func (h *TaskHandler) GetTaskByID(c *gin.Context) {
 	task, err := h.usecase.GetTaskByID(id)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
 	}
 	c.JSON(http.StatusOK, task)
 }
@@ -43,6 +46,7 @@ func (h *TaskHandler) AddTask(c *gin.Context) {
 
 	err := h.usecase.AddTask(newTask)
 	if err != nil {
+		fmt.Print("ufff", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -53,21 +57,25 @@ func (h *TaskHandler) DeleteTask(c *gin.Context) {
 	id := c.Param("id")
 	err := h.usecase.DeleteTask(id)
 	if err != nil {
-		c.JSON(500, gin.H{"err": err})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Task not found"})
+		return // Return early to avoid sending a success response
 	}
-	c.JSON(200, gin.H{"message": "successfully deleted!"})
+	c.JSON(http.StatusOK, gin.H{"message": "successfully deleted!"})
 }
 
 func (h *TaskHandler) UpdateTask(c *gin.Context) {
 	id := c.Param("id")
-	var UpdatedTask domain.Task
-	if err := c.ShouldBind(&UpdatedTask); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	var task domain.Task
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
-	err := h.usecase.UpdateTask(id, UpdatedTask)
+
+	err := h.usecase.UpdateTask(id, task)
 	if err != nil {
-		c.JSON(500, gin.H{"err": err})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Task update failed"})
+		return
 	}
-	c.JSON(200, gin.H{"message": "successfully updated!"})
+
+	c.JSON(http.StatusOK, gin.H{"message": "successfully updated!"})
 }
